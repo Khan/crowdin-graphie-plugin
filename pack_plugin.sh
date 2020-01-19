57032439dc209a4a46e25bed51a45969f2053f79
Daniel Hollas <danekhollas@gmail.com>
2020-01-20 00:20:01 +0100

    Add simple publish script,remove firefox stuff

---
+++ pack_plugin.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Creates a zip archive that can be published in chrome:
+# https://chrome.google.com/webstore/developer/dashboard
+
+# Don't forget to bump version in manifest.json before each official publish!
+version=$(grep '"version"' manifest.json | awk -F'"' '{print $4}')
+REPO_NAME=crowdin-graphie-plugin
+PACKAGE_NAME=${REPO_NAME}-${version}-${browser}
+
+cd ../
+
+rm -rf $PACKAGE_NAME.zip
+
+if [[ -e $PACKAGE_NAME ]];then
+  rm -r $PACKAGE_NAME
+fi
+
+if [[ ! -d $REPO_NAME ]];then
+   echo "ERROR: Directory $REPO_NAME does not exist!"
+   exit 1
+fi
+
+cp -r $REPO_NAME $PACKAGE_NAME
+
+cd $PACKAGE_NAME
+if [[ $? -ne 0 ]];then
+   echo "ERROR: Could not enter dir ../$PACKAGE_NAME"
+   exit 1
+fi
+
+rm -rf .git/ *.md pack_plugin.sh
+zip -r $PACKAGE_NAME.zip *
+mv $PACKAGE_NAME.zip ../
+cd ..
+
+rm -rf $PACKAGE_NAME
